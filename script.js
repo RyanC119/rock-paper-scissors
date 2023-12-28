1afef42d89dfcf98f0f92a74acd6bcede7bf6fa9
Ryan Croy <ryanc119@gmail.com>
2023-12-28 06:01:30 -0600

    Write pseudocode for a single round

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3) + 1
 
     if (randomNum === 1) {
-        computerChoice = 'Rock';
+        computerChoice = 'ROCK';
     } else if (randomNum === 2) {
-        computerChoice = 'Paper';
+        computerChoice = 'PAPER';
     } else {
-        computerChoice = 'Scissors';
+        computerChoice = 'SCISSORS';
     }
 
     return computerChoice;
 }
+
+// When the user clicks a button:
+// Store their choice in the playerSelection variable as an UPPERCASE string
+// Pass the playerSelection variable and the computerSelection function to a new function
+// Compare playerSelection to computerSelection:
+// Rock beats scissors, scissors beats paper, paper beats rock
+/// console.log "You Lose! _____ beats _____!"
+// In the case of a tie, clear out the playerSelection variable and ask the player to choose again
+// console.log "tie!"
+// Return the results of the game
+
