737f6857d7b1b553668da7d6337de5a7edc9d25d
Ryan Croy <ryanc119@gmail.com>
2023-12-27 21:18:25 -0600

    Write algorithm for getting the computer's selection

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 // Generate a random number between 1-3 inclusive
 // Assigning rock/paper/scissors to the empty variable
 // Return as the computer's choice as a string
+
+function getComputerChoice() {
+    let computerChoice;
+    let randomNum = Math.floor(Math.random() * 3) + 1
+
+    if (randomNum === 1) {
+        computerChoice = 'Rock';
+    } else if (randomNum === 2) {
+        computerChoice = 'Paper';
+    } else {
+        computerChoice = 'Scissors';
+    }
+
+    return computerChoice;
+}
