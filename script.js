d55b20ee8887116561cdaac76f28afccfcbb9178
Ryan Croy <ryanc119@gmail.com>
2023-12-27 21:12:25 -0600

    Write pseudocode for getComputerChoice function

---
+++ script.js
@@ -0,0 +1,4 @@
+// Create an empty variable to hold the computer's choice
+// Generate a random number between 1-3 inclusive
+// Assigning rock/paper/scissors to the empty variable
+// Return as the computer's choice as a string
